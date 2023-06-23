77e08e4a37bb6d293cca1efe2c8a39ccaae80854
Seun-David <oyewumidavid84@gmail.com>
2023-06-23 21:18:44 +0100

    client deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.min.js";
+import { Home } from './Pages/Home/Home';
+import { Login } from './Pages/Login/Login';
+import Jobs from './Pages/Jobs/Jobs';
+import Register from './Pages/Register/Register';
+import Contact from './Pages/Contact/Contact';
+import About from './Pages/About/About';
+import PostJob from './Pages/PostJob/PostJob';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/jobs' element={<Jobs />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/postjob' element={<PostJob />}/>
+      </Routes>
+    </>
   );
 }
 
